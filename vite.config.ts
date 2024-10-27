85fc8022a3b5152f6b422ab964531a215d946a4a
prithvikr66 <pkunofficial66@gmail.com>
2024-10-28 01:39:13 +0530

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,20 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.svg", "favicon.ico", "robots.txt", "apple-touch-icon.png"],
+      includeAssets: [
+        "favicon.svg", 
+        "favicon.ico", 
+        "robots.txt", 
+        "apple-touch-icon.png",
+        // Include all your font files
+        "fonts/ABeeZee/ABeeZee-Regular.ttf",
+        "fonts/ABeeZee/ABeeZee-Italic.ttf",
+        "fonts/sofia-pro/Sofia Pro Light Italic Az.otf",
+        "fonts/sofia-pro/Sofia Pro Light Az.otf",
+        "fonts/sofia-pro/Sofia Pro Bold Az.otf",
+        "fonts/Suisse-Intl/fonts/Suisse_Intl_Regular.ttf",
+        "fonts/Vt232/VT323-Regular.ttf"
+      ],
       manifest: {
         name: "Degen Chat",
         short_name: "DegenChat",
@@ -34,6 +47,40 @@ export default defineConfig({
           },
         ],
       },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,ttf,otf,woff,woff2}'],
+        // Add runtimeCaching for fonts
+        runtimeCaching: [
+          {
+            urlPattern: /^\/fonts\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'font-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
     }),
   ],
+  // Add this to ensure proper asset handling
+  // build: {
+  //   assetsInlineLimit: 0,
+  //   rollupOptions: {
+  //     output: {
+  //       assetFileNames: (assetInfo) => {
+  //         if (assetInfo.names.endsWith('.ttf') || assetInfo.names.endsWith('.otf')) {
+  //           return 'fonts/[name][extname]';
+  //         }
+  //         return 'assets/[name]-[hash][extname]';
+  //       },
+  //     },
+  //   },
+  // },
 });
